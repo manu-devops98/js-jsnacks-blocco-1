eaf5633822eb3c4661431677b28e6731def91971
manu-devops98 <manuelcappello7@gmail.com>
2021-11-26 13:49:30 +0100

    add snack-2

--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -1,3 +1,21 @@
 /* In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa. */
 
+const list = ['MARIO', 'CHIARA', 'FRANCESCO', 'FILIPPO', 'LUCA', 'SARA', 'GIULIA', 'FEDERICO'];
 
+let userName = prompt('Inserisci nome').toUpperCase();
+const container = document.querySelector('.container');
+let welcome = document.createElement('h1');
+
+let find = false;
+for (let i = 0; i < list.length; i++) {
+  if (userName.toUpperCase() == list[i]) {
+    find = true;
+  }
+}
+  
+  if (find) {
+    welcome = 'Benvenuto alla festa ' + userName;
+  } else {
+    welcome = 'Non sei nella lista degli invitati';
+  }
+container.append(welcome);
